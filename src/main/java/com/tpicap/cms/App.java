b4e968473fe93e9b9a152c72c1377c27689da1bc
Saqib Chaudhary <saqib@ymail.com>
2023-04-19 08:41:48 -0400

    s3 async client and logging

--- src/main/java/com/tpicap/cms/App.java
+++ src/main/java/com/tpicap/cms/App.java
@@ -1,7 +1,6 @@
 package com.tpicap.cms;
 
-
-
+import java.nio.file.Paths;
 
 /**
  * Hello world!
@@ -12,6 +11,14 @@ public class App {
     public static void main(String[] args) {
         S3Ops s3 = new S3Ops();
         s3.listBuckets();
+        String bucketName = "saqib-test-55663";
+
+        //s3.createBucket(bucketName);
+        s3.listBuckets();
+        //  s3.deleteAllObjectsInBucket(bucketName);
+        //  s3.deleteBucket(bucketName);
+        // s3.listBuckets();
+        s3.uploadFile(bucketName, "test", Paths.get("C:\\temp\\1851_3_3279076_1681671656_AWS Course Completion Certificate.pdf"));
 
     }
 }
--- src/main/java/com/tpicap/cms/S3Ops.java
+++ src/main/java/com/tpicap/cms/S3Ops.java
@@ -1,26 +1,42 @@
 package com.tpicap.cms;
 
+import java.io.File;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
 import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
 import software.amazon.awssdk.core.waiters.WaiterResponse;
 import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3AsyncClient;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.CreateBucketRequest;
-import software.amazon.awssdk.services.s3.model.S3Exception;
+import software.amazon.awssdk.services.s3.model.DeleteBucketRequest;
+import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
 import software.amazon.awssdk.services.s3.model.HeadBucketRequest;
 import software.amazon.awssdk.services.s3.model.HeadBucketResponse;
 import software.amazon.awssdk.services.s3.model.ListBucketsRequest;
 import software.amazon.awssdk.services.s3.model.ListBucketsResponse;
-import software.amazon.awssdk.services.s3.model.DeleteBucketRequest;
+import software.amazon.awssdk.services.s3.model.ListObjectsV2Request;
+import software.amazon.awssdk.services.s3.model.ListObjectsV2Response;
+import software.amazon.awssdk.services.s3.model.S3Object;
+import software.amazon.awssdk.services.s3.waiters.S3AsyncWaiter;
 import software.amazon.awssdk.services.s3.waiters.S3Waiter;
+import software.amazon.awssdk.transfer.s3.S3TransferManager;
+import software.amazon.awssdk.transfer.s3.model.FileUpload;
+import software.amazon.awssdk.transfer.s3.model.UploadFileRequest;
+import software.amazon.awssdk.transfer.s3.progress.LoggingTransferListener;
 
 public class S3Ops {
 
-    private S3Client s3 = null;
+    // https://aws.amazon.com/blogs/developer/introducing-crt-based-s3-client-and-the-s3-transfer-manager-in-the-aws-sdk-for-java-2-x/
+
+    private S3AsyncClient  s3 = null;
 
     public S3Ops() {
         ProfileCredentialsProvider credentialsProvider = ProfileCredentialsProvider.create("tpicap");
         Region region = Region.US_EAST_2;
-        s3 = S3Client.builder()
+        s3 = S3AsyncClient .builder()
                 .region(region)
                 .credentialsProvider(credentialsProvider)
                 .build();
@@ -31,8 +47,84 @@ public class S3Ops {
         ListBucketsRequest listBucketsRequest = ListBucketsRequest
                 .builder()
                 .build();
-        ListBucketsResponse listBucketsResponse = s3.listBuckets(listBucketsRequest);
-        listBucketsResponse.buckets().stream().forEach(x -> System.out.println(x.name()));
+        CompletableFuture<ListBucketsResponse> result = s3.listBuckets(listBucketsRequest);
+        result.join().buckets().stream().forEach(x -> System.out.println(x.name()));
+    }
+
+    public void createBucket(String bucketName) {
+        CreateBucketRequest bucketRequest = CreateBucketRequest.builder()
+                .bucket(bucketName)
+                .build();
+
+        s3.createBucket(bucketRequest);
+
+        S3AsyncWaiter s3Waiter = s3.waiter();
+        HeadBucketRequest bucketRequestWait = HeadBucketRequest.builder()
+                .bucket(bucketName)
+                .build();
+
+        CompletableFuture<WaiterResponse<HeadBucketResponse>> result = 
+            s3Waiter.waitUntilBucketExists(bucketRequestWait);
+        result.join().matched().response().ifPresent(System.out::println);
+        System.out.println(bucketName + " is ready");
+
+    }
+
+    public void deleteAllObjectsInBucket(String bucket) {
+
+        // To delete a bucket, all the objects in the bucket must be deleted first.
+        ListObjectsV2Request listObjectsV2Request = ListObjectsV2Request.builder()
+                .bucket(bucket)
+                .build();
+        CompletableFuture<ListObjectsV2Response> result;
+        ListObjectsV2Response listObjectsV2Response ;
+
+        do {
+            result = s3.listObjectsV2(listObjectsV2Request);
+            listObjectsV2Response = result.join();
+            List<S3Object> contents = listObjectsV2Response.contents();
+            for (S3Object s3Object : contents) {
+                DeleteObjectRequest request = DeleteObjectRequest.builder()
+                        .bucket(bucket)
+                        .key(s3Object.key())
+                        .build();
+                s3.deleteObject(request);
+            }
+        } while (listObjectsV2Response.isTruncated());
+        // snippet-end:[s3.java2.s3_bucket_deletion.delete_objects]
+
+    }
+
+    public void deleteBucket(String bucketName) {
+        DeleteBucketRequest deleteBucketRequest = DeleteBucketRequest.builder().bucket(bucketName).build();
+        s3.deleteBucket(deleteBucketRequest);
+
+        S3AsyncWaiter s3Waiter = s3.waiter();
+        HeadBucketRequest bucketRequestWait = HeadBucketRequest.builder()
+                .bucket(bucketName)
+                .build();
+
+        WaiterResponse<HeadBucketResponse> waiterResponse = s3Waiter.waitUntilBucketNotExists(bucketRequestWait).join();
+        waiterResponse.matched().response().ifPresent(System.out::println);
+        System.out.println(bucketName + " is deleted");
+    }
+
+    public void uploadFile(String bucketName, String key, Path file) {
+        // transfer manager does it all for you .....
+        // https://sdk.amazonaws.com/java/api/latest/software/amazon/awssdk/transfer/s3/S3TransferManager.html
+
+        S3TransferManager transferManager = S3TransferManager.builder()
+                .s3Client(s3)
+                .build();
+
+        UploadFileRequest uploadFileRequest = UploadFileRequest.builder()
+                .putObjectRequest(req -> req.bucket(bucketName).key(key))
+                .addTransferListener(LoggingTransferListener.create())
+                .source(file)
+                .build();
+        FileUpload upload = transferManager.uploadFile(uploadFileRequest);
+        upload.completionFuture().join();
+
     }
 
 }
